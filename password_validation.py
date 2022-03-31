798e6ce3c5e1a5bcc1396e1240d42c78533a10c3
sukhadadh <102464314+sukhadadh@users.noreply.github.com>
2022-03-31 11:48:04 +0530

    Add files via upload

---
+++ password_validation.py
@@ -0,0 +1,14 @@
+import re
+username = input("enter a username:")
+password = input("enter a password:")
+display = '*'*len(password)
+if len(password)<8:
+    print("length of password must be more than 8")
+elif " " in password:
+    print("password shouls not contain any space")
+elif not re.search('[0-9]',password):
+    print("password should contin atleats one digit")
+else:
+    print("account created successfully")
+    print("your username is:",username)
+    print("your password is:",display)
